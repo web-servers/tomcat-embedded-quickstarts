fccbe0ea4bc1892815070312ce9b396eb58a387a
Coty Sutherland <csutherl@redhat.com>
2017-02-17 15:55:59 -0500

    Adding some helpful comments to the application

--- hello-world-servlet/src/main/java/com/redhat/jws/HelloApplication.java
+++ hello-world-servlet/src/main/java/com/redhat/jws/HelloApplication.java
@@ -7,15 +7,20 @@ import org.apache.catalina.startup.Tomcat;
 
 public class HelloApplication {
     public static void main(String[] args) throws Exception {
+        // Get an instance of Tomcat
         Tomcat tomcat = new Tomcat();
 
+        // Add an empty context
         Context ctx = tomcat.addContext("", null);
 
+        // Add an instance of the servlet and add the servlet mapping
         Tomcat.addServlet(ctx, "hello", new HelloServlet());
-
         ctx.addServletMapping("/hello", "hello");
 
+        // Start the tomcat instance
         tomcat.start();
+
+        // Wait for control-C to stop the process to allow for testing
         tomcat.getServer().await();
     }
 }
