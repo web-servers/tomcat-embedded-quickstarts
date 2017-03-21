0c2e7e2f6498f1cde4cf1c6e1afafd8b836906f4
Coty Sutherland <csutherl@redhat.com>
2017-03-21 12:54:42 -0400

    Adding springboot-rest quickstart

---
+++ springboot-rest-hello-world/README.md
@@ -0,0 +1,16 @@
+# Introduction
+
+This project packages Red Hat's JWS distribution of the Apache Tomcat 8.0.36 artifacts with SpringBoot 1.4.1.RELEASE.
+
+
+# Launch
+
+     mvn spring-boot:run
+
+# Test
+
+* We can access the REST endpoint using curl or httpie tool
+```
+http http://localhost:8080/greeting
+curl http://localhost:8080/greeting
+```
---
+++ springboot-rest-hello-world/src/test/java/com/redhat/jws/quickstart/RestApplicationTest.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2016-2017 Red Hat, Inc, and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.redhat.jws.quickstart;
+
+import com.redhat.jws.quickstart.service.Greeting;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)  // Use a random port
+public class RestApplicationTest {
+
+    // This will hold the port number the server was started on
+    @Value("${local.server.port}")
+    int port;
+
+    final RestTemplate template = new RestTemplate();
+
+    @Test
+    public void callServiceTest() {
+        Greeting message = template.getForObject("http://localhost:" + port + "/greeting",Greeting.class);
+        Assert.assertEquals("Hello, World!", message.getContent());
+        Assert.assertEquals(1, message.getId());
+    }
+
+}
